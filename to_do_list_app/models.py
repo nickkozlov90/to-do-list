9fa8470204fe8acc8433fbbccb1a829e4fc20294
Mykola <nickkozlov90@gmail.com>
2023-10-16 19:12:48 +0300

    Create models

--- to_do_list_app/models.py
+++ to_do_list_app/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+
+class Tag(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return f"{self.name}"
+
+
+class Task(models.Model):
+    content = models.CharField(max_length=255)
+    created = models.DateTimeField(auto_now_add=True)
+    deadline = models.DateTimeField(null=True)
+    is_done = models.BooleanField()
+    tags = models.ManyToManyField(related_name="tasks")
+
+    class Meta:
+        ordering = ["is_done", "-created"]
+
+    def __str__(self):
+        return f"{self.content}"
