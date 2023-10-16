d768a7a155a090d4b653422ac11a93f1c472c42b
Mykola <nickkozlov90@gmail.com>
2023-10-16 21:59:17 +0300

    Create task change status view

--- to_do_list_app/views.py
+++ to_do_list_app/views.py
@@ -1,4 +1,6 @@
+from django.http import request, HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse_lazy
 from django.views import generic
 
 from to_do_list_app.models import Task
@@ -12,7 +14,6 @@ class TaskListView(generic.ListView):
     model = Task
     context_object_name = "task_list"
     # queryset = Task.objects.all().prefetch_related("")
-
 
 
 class TaskCreateView(generic.CreateView):
@@ -32,6 +33,9 @@ class TagListView(generic.ListView):
     pass
 
 
-def change_task_status():
-    pass
+def change_task_status(request, pk):
+    task = Task.objects.get(id=pk)
+    task.is_done = not task.is_done
+    task.save()
+    return HttpResponseRedirect(reverse_lazy("to_do_list_app:task-list"))
 
